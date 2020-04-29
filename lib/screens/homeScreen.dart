828a508c4fcd4abfad443a8678a5dcbf1361fef6
ggutierrez <ggutierrez@grainchain.io>
2020-04-28 22:16:24 -0500

    Adding two ListView

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -9,36 +9,6 @@ class HomeScreen extends StatelessWidget {
     print('Hola Mundo');
   }
 
-  
-
-  // Container getContainer() {
-  //   return Container(
-  //     color: Colors.redAccent,
-  //     margin: EdgeInsets.all(10),
-  //     child: Row(
-  //       mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //       children: <Widget>[
-  //         getSonContainer(),
-  //         getSonContainer(),
-  //         getSonContainer(),
-  //       ]
-  //     ),
-  //   );
-  // }
-
-  // Expanded getElements(BuildContext context) {
-  //   return Expanded(
-  //     flex: 1,
-  //     child: Container(
-  //       width: double.infinity,
-  //       decoration: BoxDecoration(
-  //         border: Border.all(color: Colors.red, width: 5),
-  //         color: Colors.pinkAccent,
-  //       ),
-  //       child: InkWell(child: Text('Navigate'), onTap: () => handleTap(context),)
-  //     ),
-  //   );
-  // }
  InkWell getItemList(text, color) {
    return InkWell(
      onTap: () => print(text),
@@ -81,9 +51,35 @@ class HomeScreen extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: <Widget>[
-                getItemList('Drama', Colors.red),
-                getItemList('Drama', Colors.red),
-                getItemList('Drama', Colors.red)
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 16),
+                  padding: EdgeInsets.all(20),
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.orangeAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: Text(
+                    'The Best Movies',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                    )
+                  )
+                ),
+                Container(
+                  height: 300,
+                  margin: EdgeInsets.only(top: 16),
+                  child: ListView(
+                    children: <Widget>[
+                      getItemList('Drama', Colors.red),
+                      getItemList('Terror', Colors.blue),
+                      getItemList('Commedy', Colors.green),
+                      getItemList('Science Fiction', Colors.purple),
+                    ]
+                  )
+                )
               ]
             ),
           )
@@ -92,23 +88,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
-
-
-//  Container(
-//               width: double.infinity,
-//               margin: EdgeInsets.only(top: 16),
-//               padding: EdgeInsets.all(20),
-//               height: 150,
-//               decoration: BoxDecoration(
-//                 color: Colors.orangeAccent,
-//                 borderRadius: BorderRadius.all(Radius.circular(10))
-//               ),
-//               child: Text(
-//                 'The Best Movies',
-//                 style: TextStyle(
-//                   fontWeight: FontWeight.bold,
-//                   color: Colors.white
-//                 )
-//               )
-//             ),
